f41bee82dcea3c88b2be85c1708ecd1428c27f0d
huifer <huifer97@163.com>
2020-09-25 09:15:52 +0800

    project init

---
+++ event-parent/event-common/src/main/java/com/github/huifer/domain/common/model/EventModel.java
@@ -0,0 +1,10 @@
+package com.github.huifer.domain.common.model;
+
+import java.time.LocalDateTime;
+
+public abstract class EventModel {
+	private String uid;
+
+	private LocalDateTime createAt;
+
+}
